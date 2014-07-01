aa9f9cceabb8e2f146727f155bb655ea59df230a
Tatyana <danya_ca@yahoo.com>
2014-06-30 19:15:26 -0700

    added down button and updated other stuff

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
-    
+    $('.clickDown').click(function() {
+        $.fn.fullpage.moveSectionDown();
+
+    });
 });
+
+    //Click on the down button and move to another section
+//     $('#down-button').click(function() {
+//          $('#down-button').scroll() 
+//     });
+// });
+
+
+
+
+// function top() {
+//     document.getElementById( 'top' ).scrollIntoView();    
+// };
+
+// function bottom() {
+//     document.getElementById( 'bottom' ).scrollIntoView();
+//     window.setTimeout( function () { top(); }, 2000 );
+// };
+
+// bottom();
+
