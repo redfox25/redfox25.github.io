44972c48e7c632d5a5ffb333a8f0a3d208ec0dc5
Kate Heddleston <kate@runscope.com>
2014-07-07 16:55:31 -0700

    Side nav working but not hiding properly.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($){
+	var $lateral_menu_trigger = $('#cd-menu-trigger'),
+		$content_wrapper = $('#fullpage'),
+		$navigation = $('header');
+
+	//open-close lateral menu clicking on the menu icon
+	$lateral_menu_trigger.on('click', function(event){
+		event.preventDefault();
+		
+		$lateral_menu_trigger.toggleClass('is-clicked');
+		$navigation.toggleClass('lateral-menu-is-open');
+		$('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+		
+		//check if transitions are not supported - i.e. in IE9
+		if($('html').hasClass('no-csstransitions')) {
+			$('body').toggleClass('overflow-hidden');
+		}
+	});
+
+	//close lateral menu clicking outside the menu itself
+	$content_wrapper.on('click', function(event){
+		if( !$(event.target).is('#cd-menu-trigger, #cd-menu-trigger span') ) {
+			$lateral_menu_trigger.removeClass('is-clicked');
+			$navigation.removeClass('lateral-menu-is-open');
+			$('#cd-lateral-nav').removeClass('lateral-menu-is-open');
+			//check if transitions are not supported
+			if($('html').hasClass('no-csstransitions')) {
+				$('body').removeClass('overflow-hidden');
+			}
+
+		}
+	});
+});
--- script.js
+++ script.js
@@ -2,27 +2,6 @@ $(document).ready(function() {
     $('#fullpage').fullpage();
     $('.clickDown').click(function() {
         $.fn.fullpage.moveSectionDown();
-
     });
+    
 });
-
-    //Click on the down button and move to another section
-//     $('#down-button').click(function() {
-//          $('#down-button').scroll() 
-//     });
-// });
-
-
-
-
-// function top() {
-//     document.getElementById( 'top' ).scrollIntoView();    
-// };
-
-// function bottom() {
-//     document.getElementById( 'bottom' ).scrollIntoView();
-//     window.setTimeout( function () { top(); }, 2000 );
-// };
-
-// bottom();
-
